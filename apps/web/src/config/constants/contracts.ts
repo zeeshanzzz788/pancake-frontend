abc5b120e585a7cd773a7e5a3dd34de3f6df5ef3
Zeeshan <zk706026@gmail.com>
2026-03-15 13:28:27 +0500

    Create contracts.ts

---
+++ apps/web/src/config/constants/contracts.ts
@@ -0,0 +1,9 @@
+export const contracts = {
+  97453: {
+    router: '0x35cAC72Db00e8dAC0e4f7F8A0F53D339E0cC23fb',
+    quoter: '0xe24929ec26cc2faD91655B3AD3C597Fd477A80F9',
+    factory: '0xCFE41fb5dA87916D84E7F2289087b4Ff7163cDE',
+    nonfungiblePositionManager: '0x8b9bCc8C722778f30146e20e44E8d8e28adD8df8',
+    weth: '0xe4095a910209d7be03b55d02f40d4554b1666182',
+  },
+} as const
