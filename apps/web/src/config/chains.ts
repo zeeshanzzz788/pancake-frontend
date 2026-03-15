5a67174be7f67f53dc679244e77a11fa7348ef4f
Zeeshan <zk706026@gmail.com>
2026-03-15 13:40:57 +0500

    Update chains.ts

--- apps/web/src/config/chains.ts
+++ apps/web/src/config/chains.ts
@@ -1,23 +1,19 @@
 import { ChainId } from '@pancakeswap/sdk'
 
-export enum SupportedChainId {
-  SIDRA = 97453,
-}
-
-export const CHAIN_IDS_TO_NAMES = {
-  [SupportedChainId.SIDRA]: 'SidraChain',
+export const SIDRA = {
+  id: 97453,
+  name: 'SidraChain',
+  nativeCurrency: { name: 'SDA', symbol: 'SDA', decimals: 18 },
+  rpcUrls: { default: { http: ['https://node.sidrachain.com'] } },
+  blockExplorers: { default: { url: 'https://ledger.sidrachain.com' } },
 } as const
 
-export const DEFAULT_CHAIN_ID = SupportedChainId.SIDRA
+export const CHAINS = [SIDRA] as const
 
-export const SUPPORTED_CHAINS = [SupportedChainId.SIDRA] as const
-
-export const CHAIN_INFO = {
-  [SupportedChainId.SIDRA]: {
-    chainId: SupportedChainId.SIDRA,
-    name: 'SidraChain',
-    nativeCurrency: { name: 'SDA', symbol: 'SDA', decimals: 18 },
-    rpcUrls: { default: { http: ['https://node.sidrachain.com'] } },
-    blockExplorerUrls: ['https://ledger.sidrachain.com'],
-  },
+export const CHAIN_QUERY_NAME = {
+  [SIDRA.id]: 'sidra',
 } as const
+
+export const DEFAULT_CHAIN_ID = 97453
+
+export const SUPPORTED_CHAINS = [SIDRA] as const
